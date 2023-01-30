921fbd02c2f2d9d568f0e8fb6d854e6d4a5fc11e
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-30 13:08:22 +0700

    Add basic layout of html, css, and js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+
+  myLibrary.push(input.value);
+}
+
+function displayBooks(){
+    for(let i=0;i<myLibrary.length;i++){
+        return myLibrary[i];
+    }
+}
+
